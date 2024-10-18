bed656d441add34821d9c2e762c37024b3712854
VietDH15 <vietdh15@fpt.com>
2024-10-18 16:55:07 +0700

    them df

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,8 +1,27 @@
 from churn_prediction.data.dataset import read_dataset
 import pytest
 import logging
+import pandas as pd
 
 logger = logging.getLogger()
+
+@pytest.fixture
+def sample_data():
+    sample = pd.DataFrame({
+        "CreditScore": [],
+        "Geography": ['France', 'Spain', 'Germany'],
+        "Gender": [],
+        "Age": [],
+        "Tenure": [],
+        "Balance": [],
+        "NumOfProducts": [],
+        "HasCrCard": [],
+        "IsActiveMember": [],
+        "EstimatedSalary": [],
+        "Exited": []
+    })
+
+    return sample
 
 def test_read_dataset():
     with pytest.raises(AssertionError, match="We only support csv files for now"):
